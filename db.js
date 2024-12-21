58745789984947560804dc57dc72bb549f887796
Abhishek AN <abhishekan@Abhisheks-MacBook-Air.local>
2024-12-21 20:26:30 +0530

    update

--- db.js
+++ db.js
@@ -1,44 +1,30 @@
-const { Pool } = require('pg');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'logindb',
-    password: 'root',
-    port: 5432,
-});
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connectDB = async () => {
+    if (!client.isConnected()) await client.connect();
+    return client.db('logindb');
+};
 
 const createUser = async (name, email, password) => {
-    const client = await pool.connect();
-    try {
-        const result = await client.query(
-            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
-            [name, email, password]
-        );
-        return result.rows[0];
-    } finally {
-        client.release();
-    }
+    const db = await connectDB();
+    const result = await db.collection('users').insertOne({ name, email, password });
+    return result.ops[0];
 };
 
 const getUserByEmail = async (email) => {
-    const client = await pool.connect();
-    try {
-        const result = await client.query('SELECT * FROM users WHERE email = $1', [email]);
-        return result.rows[0];
-    } finally {
-        client.release();
-    }
+    const db = await connectDB();
+    const user = await db.collection('users').findOne({ email });
+    return user;
 };
 
 const getUserById = async (id) => {
-    const client = await pool.connect();
-    try {
-        const result = await client.query('SELECT * FROM users WHERE id = $1', [id]);
-        return result.rows[0];
-    } finally {
-        client.release();
-    }
+    const db = await connectDB();
+    const user = await db.collection('users').findOne({ _id: new MongoClient.ObjectID(id) });
+    return user;
 };
 
 module.exports = {
@@ -46,3 +32,7 @@ module.exports = {
     getUserByEmail,
     getUserById,
 };
+
+
+
+// BlrPU7xUlY0WybIC
