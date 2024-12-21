bb882eb7873b0ac82972fa407836299c5ebee605
Abhishek AN <abhishekan@Abhisheks-MacBook-Air.local>
2024-12-21 19:55:00 +0530

    path update

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ const { createUser, getUserByEmail, getUserById } = require('./db');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://ai-resume-analyser-lnsb0vo1x-abhishek-ans-projects-83378fc9.vercel.app/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
